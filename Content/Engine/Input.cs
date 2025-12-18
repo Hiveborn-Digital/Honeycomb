f2024f63efc2a0f5d64f2ff2a80a31300c4d7265
Daxcr <daxaroodles@gmail.com>
2025-12-19 12:18:19 +1300

    basic input api for key pressing/releasing. will get mouse/controller/binding working soon

---
+++ Content/Engine/Input.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace HoneycombEngine
+{
+    public partial class Honeycomb
+    {
+        public class Input
+        {
+            public static KeyboardState KeyboardState = Keyboard.GetState();
+            //public static GamePadState GamePadState = GamePad.GetState();
+            public static MouseState MouseState = Mouse.GetState();
+
+            private static KeyboardState PreviousKeyboardState = Keyboard.GetState();
+            private static MouseState PreviousMouseState = Mouse.GetState();
+            public static void Update()
+            {
+                PreviousKeyboardState = KeyboardState;
+                PreviousMouseState = MouseState;
+                KeyboardState = Keyboard.GetState();
+                MouseState = Mouse.GetState();
+            }
+            public static bool KeyPressed(Keys key)
+            {
+                return KeyboardState.IsKeyDown(key) && !PreviousKeyboardState.IsKeyDown(key);
+            }
+            public static bool KeyReleased(Keys key)
+            {
+                return !KeyboardState.IsKeyDown(key) && PreviousKeyboardState.IsKeyDown(key);
+            }
+            public static bool KeyDown(Keys key)
+            {
+                return KeyboardState.IsKeyDown(key);
+            }
+        }
+    }
+}
