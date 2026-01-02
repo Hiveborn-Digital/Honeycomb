dc4dec235267b9549a11cc50b06765ef128ec832
Daxcr <daxaroodles@gmail.com>
2026-01-02 17:34:06 +1300

    Splitting rendering into its own project (ManukaRenderer) to help keep things separate and clean.

---
+++ ManukaRenderer/Core.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ManukaRendererCore
+{
+    public class ManukaCore : Game
+    {
+        public ManukaCore() { }
+    }
+}
---
+++ ManukaRenderer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ManukaRendererInit
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            new ManukaRendererCore.ManukaCore();
+        }
+    }
+}
